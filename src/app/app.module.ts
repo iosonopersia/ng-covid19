de391f61a3dc44a6aeda1688ec81a38e86e5299f
Simone Persiani <iosonopersia@gmail.com>
2020-04-20 18:40:34 +0200

    Update App module and import NgbModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { SharedStateService } from './services/SharedState/shared-state.service';
+import { SEPAQueriesService } from './services/SEPA/SEPAQueries/sepaqueries.service';
 import { SEPASubscriptionsService } from './services/SEPA/SEPASubscriptions/sepasubscriptions.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,8 +11,8 @@ import { MapComponent } from './components/map/map.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PlotComponent } from './components/plot/plot.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { PageComponent } from './components/shared/page/page.component';
+import { HeaderComponent } from './pages/shared/header/header.component';
+import { PageComponent } from './pages/shared/page/page.component';
 
 // La distro 'basic' di plotly mette a disposizione solamente 'scatter', 'bar' e 'pie'
 import { PlotlyViaWindowModule } from 'angular-plotly.js';
@@ -27,10 +30,13 @@ import { PlotlyViaWindowModule } from 'angular-plotly.js';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PlotlyViaWindowModule
+    PlotlyViaWindowModule,
+    NgbModule
   ],
   providers: [
-    SEPASubscriptionsService
+    SEPASubscriptionsService,
+    SEPAQueriesService,
+    SharedStateService
   ],
   bootstrap: [AppComponent]
 })
