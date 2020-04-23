1b2957a254a12b377d7b669561b6fa527e94a492
Simone Persiani <iosonopersia@gmail.com>
2020-04-23 10:58:45 +0200

    Fix Dashboard component

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-  links = [
-    { title: 'Query', path: 'query' },
-    { title: 'Subscribe', path: 'subscribe' },
-    { title: 'Help', path: 'help' }
+export class DashboardComponent implements OnInit, OnDestroy {
+  tabs = [
+    { name: 'Query', url: '/dashboard/query' },
+    { name: 'Subscribe', url: '/dashboard/subscribe' },
+    { name: 'Help', url: '/dashboard/help' }
   ];
+  activeTabUrl: string;
+  private subscriptions: Subscription;
 
-  constructor(public route: ActivatedRoute) {}
-
-  ngOnInit(): void {
+  constructor(public router: Router) {
+    this.activeTabUrl = this.router.url;
+    this.subscriptions = new Subscription();
   }
 
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          this.activeTabUrl = event.urlAfterRedirects;
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
