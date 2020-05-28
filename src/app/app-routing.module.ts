958bfb847d35e58bd37fa99f912e74ae9acd7312
Simone Persiani <iosonopersia@gmail.com>
2020-05-29 00:12:40 +0200

    Add routing config for Superset dashboards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { QueryComponent } from './pages/query/query.component';
 import { HelpComponent } from './pages/help/help.component';
+import { SupersetComponent } from './pages/superset/superset.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'query', component: QueryComponent },
   { path: 'help', component: HelpComponent },
+  {
+    path: 'covidFree',
+    component: SupersetComponent,
+    data: { dashboardName: 'covidFreeDashboard' },
+  },
+  {
+    path: 'airQuality',
+    component: SupersetComponent,
+    data: { dashboardName: 'airQualityDashboard' },
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
